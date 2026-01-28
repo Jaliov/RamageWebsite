2c5cc062ac0b881063a9854442400dc5ceabcf19
Joel Rudin <jarud55@gmail.com>
2026-01-27 23:57:40 -0500

    app.js css in progress

---
+++ app.js
@@ -0,0 +1,28 @@
+
+//selecting ul
+const gallery = document.querySelector(".gallery")
+
+//select imagebox
+const itemboxes = document.querySelectorAll(".itembox")
+console.log(itemboxes)
+
+
+gallery.addEventListener("click", (e) => {
+    console.log(e)
+
+
+   if(e.target.classList.contains("filter")) {
+            gallery.querySelector(".active").classList.remove("active");
+            e.target.classList.add("active");
+
+            const filterValue = e.target.getAttribute("data-filter");
+            console.log(filterValue)
+
+            itemboxes.forEach(item =>{
+                if(item.classList.contains(filterValue) || filterValue === "all") {
+                    item.classList.add("show")
+                }
+            }
+)}
+})
+
