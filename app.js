95c1112bf71d4990375c5e344e4102baab1289a3
Joel Rudin <jarud55@gmail.com>
2026-02-05 13:02:37 -0500

    facnybox removed,style edits

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
-
 //selecting ul
 const gallery = document.querySelector(".gallery")
 
@@ -35,18 +34,50 @@ const artCat = document.querySelector('.art_category')
   
     console.log(filterValue)
    if (filterValue === "hankie_art") {
-    artCat.innerHTML = '<a href="hankiart.html">More hankie art</a>';
+    artCat.innerHTML = '<a href="hankiart.html" style="text-decoration: none;color:black;" class="filter" >More hankie art</a>';
    }
    else if(filterValue === "rules_of_game") {
-    artCat.innerHTML = '<a href="rules.html">More rules of the game</a>';
+    artCat.innerHTML = '<a href="rules.html" style="text-decoration: none;color:black;" class="filter">More rules of the game</a>';
+   }
+   else {
+    artCat.innerHTML = '';
    }
     
-    
+   artCat.addEventListener('mouseover', () => {
+  // Use camelCase for hyphenated CSS properties (e.g., 'backgroundColor')
+   artCat.style.backgroundColor = '#e84545';
+   artCat.style.color = 'yellow';
+});
+  artCat.addEventListener('mouseout', () => {
+  // Revert styles on mouse out
+  artCat.style.backgroundColor = 'initial';
+  artCat.style.color = 'initial';
+});
    
 
 })
-// Source - https://stackoverflow.com/a/3230204
-// Posted by 2ndkauboy
-// Retrieved 2026-02-04, License - CC BY-SA 2.5
+// Get the modal, image, and caption elements
 
+
+// When the user clicks the small image, open the modal and display the clicked image
+img.onclick = function(){
+  modal.style.display = "block";
+  modalImg.src = this.src;
+  captionText.innerHTML = this.alt;
+}
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// Optional: Close the modal if the user clicks anywhere outside of the image
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
 
