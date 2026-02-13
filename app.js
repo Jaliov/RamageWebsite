de39a705cab4796a4c5242481210aad43a9ce447
Joel Rudin <jarud55@gmail.com>
2026-02-12 19:44:41 -0500

    styling links

--- app.js
+++ app.js
@@ -25,18 +25,22 @@ const filterValue = e.target.getAttribute("data-filter");
             }         
 )}
 var link = document.createElement('link');
-
 let artCat = document.querySelector('.art_category')
 let art_cat = document.querySelector('.art_cat')
 
     console.log(filterValue)
    if (filterValue === "hankie_art") {
     demonsText.innerHTML = "<p>This is a description of hankie art</p>"
-    artCat.innerHTML = '<a href="hankiart.html" class="filter" >More hankie art &rarr;</a>';
+    artCat.innerHTML = '<a href="hankiart.html"  class="button" >More hankie art &rarr;</a>';
+    art_cat.innerHTML = "Hankie Art";
+     const element = document.querySelector('.all_page');
+      if (element) {
+        element.remove();
+          }
    }
    else if(filterValue === "rules_of_game") {
     demonsText.innerHTML = "<p>This is a description of rules of the game</p><br>"
-    artCat.innerHTML = '<a href="rules.html"  >More rules of the game&rarr;</a>'; 
+    artCat.innerHTML = '<a href="rules.html" class="button">More rules of the game&rarr;</a>'; 
 
     art_cat.innerHTML = "Rules of the Game";
     const element = document.querySelector('.all_page');
@@ -46,9 +50,27 @@ let art_cat = document.querySelector('.art_cat')
    }
    else if(filterValue === "hive_mind") {
     demonsText.innerHTML = "<p>This is a description of hive mind</p><br>"
-    artCat.innerHTML = '<a href="hive_mind.html"  >More hive mind &rarr;</a>'; 
-
+    artCat.innerHTML = '<a href="hive_mind.html" class="button">More hive mind &rarr;</a>'; 
+    
     art_cat.innerHTML = "Hive Mind";
+    const element = document.querySelector('.all_page');
+      if (element) {
+        element.remove();
+          }
+   }
+   else if(filterValue === "ghosts") {
+    demonsText.innerHTML = "<p>This is a description of ghosts</p><br>"
+    artCat.innerHTML = '<a href="ghosts.html" class ="button " >More ghosts &rarr;</a>'; 
+
+    art_cat.innerHTML = "Ghosts";
+    const element = document.querySelector('.all_page');
+      if (element) {
+        element.remove();
+          }
+   }
+    else if(filterValue === "demons") {
+  
+    art_cat.innerHTML = "Demons";
     const element = document.querySelector('.all_page');
       if (element) {
         element.remove();
