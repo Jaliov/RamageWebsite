535714bd1b8ff7e21c0e96b09ee83dcc4868c7f6
Joel Rudin <jarud55@gmail.com>
2026-02-26 12:35:32 -0500

    fixed About.html js file link

--- js/modal.js
+++ js/modal.js
@@ -1,17 +1,11 @@
-
-function openModal(modalId, caption) {
-  let modal = document.getElementById(modalId);
-  modal.style.display = "flex";
-  modal.classList.add("show");
-  let message = modal.querySelector(".caption");
-  message.innerText = caption;
-}
-
-function closeModal(modalId) {
-  let modal = document.getElementById(modalId);
-  modal.classList.remove("show");
-  setTimeout(function () {
-    modal.style.display = "none";
-    modal.querySelector(".caption").innerText = "";
-  }, 300);
-}
+const modal = document.getElementById("my-modal");
+const openModalBtn = document.getElementById("open-modal-btn");
+const closeModalBtn = document.getElementById("close-modal-btn");
+// When the user clicks the open button, open the modal as a modal dialog
+openModalBtn.addEventListener("click", () => {
+  modal.showModal();
+});
+// When the user clicks the close button, close the modal
+closeModalBtn.addEventListener("click", () => {
+  modal.close();
+});
