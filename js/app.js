9859aaf4b0b1d66d7b27144e90c05afaf675fb50
Joel Rudin <jarud55@gmail.com>
2026-03-29 21:04:58 -0400

    scrollup edits/add js

--- js/app.js
+++ js/app.js
@@ -123,11 +123,8 @@ const blurHeader = () => {
     const header = document.getElementById('header')
     
     // When the scroll is greater than 50 viewport height, add the blur-header class to the header tag
-    this.scrollY >= 60 ? header.classList.add('blur-header')
+    this.scrollY >= 500 ? header.classList.add('blur-header')
                       :header.classList.remove('blur-header')
 }
 window.addEventListener('scroll', blurHeader)
-
-
-
 
--- js/modal.js
+++ js/modal.js
@@ -17,3 +17,13 @@ openModalBtn.addEventListener("click", () => {
 closeModalBtn.addEventListener("click", () => {
   modal.close();
 });
+
+  /*=============== SHOW SCROLL UP ===============*/ 
+const scrollUp = () => {
+    const scrollUp = document.getElementById('scroll-up')
+      //When the scroll is higher than 350 viewport height, add the show-scroll class to 
+      this.scrollY >= 350 ? scrollUp.classList.add('show-scroll')
+                    : scrollUp.classList.remove('show-scroll')   
+}
+window.addEventListener('scroll', scrollUp)
+
